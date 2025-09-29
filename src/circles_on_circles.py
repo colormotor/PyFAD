541f88deb1c7335534bf05571905bd2bb7ef66b9
colormotor <drand48@gmail.com>
2025-09-29 14:53:45 +0100

    2025-26 cleanup

--- src/circles_on_circles.py
+++ src/circles_on_circles.py
@@ -6,22 +6,22 @@ def setup():
 def draw_circle_on_circle(layout_angle, center, radius):
     begin_shape()
     for angle in linspace(0, TWO_PI, 5, False):
-        r = radius  #remap(sin(angle*3 + layout_angle*2), -1, 1, radius*0.5, radius)        
+        r = remap(sin(angle*2 + layout_angle*4), -1, 1, radius*0.4, radius)        
         p = direction(angle)*r + center
-        vertex(p)
+        curve_vertex(p)
     end_shape(CLOSE)
 
 def draw():
     t = frame_count / 1000
     
-    background(0)
+    background(255)
     no_fill()
-    stroke(255, 128)
+    stroke(0, 128)
     
     translate(width / 2, height / 2)
     
-    num_circles = 110
-    layout_radius = width*0.3
+    num_circles = 170
+    layout_radius = width*0.2
     circle_radius = 250
 
     # Iterate over angles that cover a circle (0 to TWO_PI)
